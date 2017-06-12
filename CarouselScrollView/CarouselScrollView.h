4901f31f42a4928b83941d41b3643c7535e5577a
Gabriel Valério <gbrvalerio@Mac311.local>
2017-06-12 10:05:55 -0300

    Initial Commit

---
+++ CarouselScrollView/CarouselScrollView.h
@@ -0,0 +1,19 @@
+//
+//  CarouselScrollView.h
+//  CarouselScrollView
+//
+//  Created by Gabriel Bezerra Valério on 12/06/17.
+//  Copyright © 2017 bepiducb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarouselScrollView.
+FOUNDATION_EXPORT double CarouselScrollViewVersionNumber;
+
+//! Project version string for CarouselScrollView.
+FOUNDATION_EXPORT const unsigned char CarouselScrollViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarouselScrollView/PublicHeader.h>
+
+
